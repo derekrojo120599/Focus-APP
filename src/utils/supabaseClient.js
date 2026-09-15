3704cbe112dc50d8617b5e77362bfaec91df1e3f
Derek Rojo <derekrojo@gmail.com>
2026-09-14 23:27:28 -0400

    cambios en vite

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Usamos las claves directas pa' evitar rollos con el .env y Vite
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://xmhgruhiyzoqqfpdgzhk.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtaGdydWhpeXpvcXFmcGRnemhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0MzkwODgsImV4cCI6MjEwNTAxNTA4OH0.oEcggng7Mbd-a79r9AZlF1H88DKSOUPp8XhuC25-Sys';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
